8a77e9bb6d5568c1fc56b03eb0527f2970e5f34a
Serj <serj.dub1na@gmail.com>
2023-04-23 21:48:22 +0300

    Update ws_proto_test.py

--- ws_proto_test.py
+++ ws_proto_test.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+# Run pip3 install websockets to install the websockets library
+# Run pip3 install protobuf to install the protobuf library
+# 'export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' to use the python implementation of protobuf
+# Run 'protoc -I=. --python_out=. protos/sigmiot_data.proto' to generate the python code from the proto file
+
 import asyncio
 import websockets
 
-import sigmiot_data_pb2
+from protos import sigmiot_data_pb2
 from dataclasses import dataclass
 
 @dataclass
@@ -28,7 +33,7 @@ def create_sensor_data(sensor_name, sensor_type, sensor_location, sensor_values)
     return sensor_data
 
 def append_sensor_data_to_message(message, sensor):
-    sensors_data = sigmiot_data_pb2.SensorData()
+    sensors_data = sigmiot_data_pb2.SensorDataResponse()
     sensors_data.sensor_name = sensor.sensor_name
     sensors_data.sensor_type = sensor.sensor_type
     sensors_data.sensor_location = sensor.sensor_location
@@ -40,7 +45,7 @@ def append_sensor_data_to_message(message, sensor):
         sensor_values.value_unit = sensor_value.value_unit
         sensors_data.sensor_values.extend([sensor_values])
 
-    message.sensor_data_response.sensors_data.extend([sensors_data])
+    message.sensors_data_response.extend([sensors_data])
     return message
 
 def append_log_data_to_message(message, log_message, log_timestamp, log_level):
@@ -55,12 +60,12 @@ def print_message(message):
     serialized_message = message.SerializeToString()
 
     # Deserialize the message from bytes
-    # Parse a received message from bytes
     received_message = sigmiot_data_pb2.MessageResponse()
+    # Parse a received message from bytes
     received_message.ParseFromString(serialized_message)
 
     # Access the fields of the received message
-    for sensor_data in received_message.sensor_data_response.sensors_data:
+    for sensor_data in received_message.sensors_data_response:
         print(sensor_data.sensor_name)
         print(sensor_data.sensor_type)
         print(sensor_data.sensor_location)
@@ -76,6 +81,8 @@ def print_message(message):
 
 async def message_sender(websocket, path):
     message = sigmiot_data_pb2.MessageResponse()
+
+    message.status = sigmiot_data_pb2.MessageResponse.Status.OK
 
     sensor1_value1 = create_sensor_value("temperature", 1.0, "C")
     sensor1_value2 = create_sensor_value("humidity", 2.0, "%")
@@ -106,9 +113,8 @@ async def handle_connection(websocket, path):
     await message_sender(websocket, path)
 
 if __name__ == "__main__":
-    message = sigmiot_data_pb2.MessageResponse()
-
-    print_message(message)
+    print("Starting WebSocket server")
+    print("Press Ctrl+C to stop the server")
 
     # Start the WebSocket server
     asyncio.get_event_loop().run_until_complete(
